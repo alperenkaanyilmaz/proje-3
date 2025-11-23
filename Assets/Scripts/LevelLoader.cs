bdb51d0ea29c8c7332a1e1d2e8aa6cd13a1a1cda
Alperen Kaan Yılmaz <kaanyilmaz38010@gmail.com>
2025-11-24 02:08:17 +0300

    Update LevelLoader.cs

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -1,1 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
+public class LevelLoader : MonoBehaviour
+{
+    public static LevelLoader Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this) { Destroy(gameObject); return; }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public IEnumerator LoadLevelAsync(string levelName)
+    {
+       
+        AsyncOperation op = SceneManager.LoadSceneAsync(levelName);
+        while (!op.isDone) yield return null;
+    }
+}
